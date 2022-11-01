ee5d9ffc823f4f6162be798fd73cfe357b795dba
fgiannini <fgiannini@diism.unisi.it>
2022-11-01 22:25:32 +0100

    F: updates with JSON

--- Lattice_Generator.py
+++ Lattice_Generator.py
@@ -1,33 +1,44 @@
 import numpy as np
 import itertools
-import networkx as nx
-import matplotlib.pyplot as plt
-from networkx import nx_pydot
-from utils import *
+from utils import is_distributive, is_transitive, is_modular
+import json
 
-number_of_lattices_to_generate = 9
-
+lattices_to_generate = 10 # 10
+max_lattices_cardinality = 8
 
 
+#TODO
+def LoQ2Adj(mat):
+    n = np.size(mat[0])
 
-def generate_LoQ_matrices(n,domain_pairs,assignments, sampling):
+    adjacency_matrix = np.zeros([n,n])
+    return adjacency_matrix
+
+def generate_LoQ_matrices(n,domain_pairs, sampling, lattices_to_generate):
     matrices_list = []
 
+    # number of pairs to define possible functions
+    num_of_pairs = len(domain_pairs)
+
     if sampling:
-        random_assignments = np.random.rand(assignments)[:number_of_lattices_to_generate]
+        tuple_taken = len(matrices_list)
+        while tuple_taken < lattices_to_generate:
+            candidate = np.random.choice([0,1], size=num_of_pairs)
 
-        for a in random_assignments:
-            new_matrix = np.triu(np.ones([n, n]))
-
+            new_matrix = np.triu(np.ones([n, n])) # , k=1
+            # new_matrix = np.triu(np.ones([n, n]), k=1)
             for j, p in enumerate(domain_pairs):
-                new_matrix[p[0], p[1]] = a[j]
-
-            if is_transitive((new_matrix)):
+                new_matrix[p[0], p[1]] = candidate[j]
+            if is_transitive(new_matrix):
+                # if new_matrix not in matrices_list:
                 matrices_list.append(new_matrix)
+                tuple_taken += 1
 
     else:
+        assignments = itertools.product([0, 1], repeat=num_of_pairs)
         for a in assignments:
-            new_matrix = np.triu(np.ones([n,n]))
+            new_matrix = np.triu(np.ones([n,n])) # , k=1
+            # new_matrix = np.triu(np.ones([n,n]), k=1)
 
             for j, p in enumerate(domain_pairs):
                 new_matrix[p[0],p[1]] = a[j]
@@ -35,60 +46,57 @@ def generate_LoQ_matrices(n,domain_pairs,assignments, sampling):
             if is_transitive((new_matrix)):
                 matrices_list.append(new_matrix)
 
-    return(matrices_list)
+    matrices_list = list(matrices_list)
 
-
-
-def generate_all_lattices(n):
-
-    domain_pairs = []
-    x = 1
-    while x < n-1:
-        y = x + 1
-        while y < n-1:
-            domain_pairs.append([x,y])
-            y += 1
-        x += 1
-
-    num_of_pairs = len(domain_pairs)
-    assignments = itertools.product([0,1],repeat=num_of_pairs)
-
-    if n >= 10:
-        sampling = True
-    else:
-        sampling = False
-
-    matrices_list = generate_LoQ_matrices(n,domain_pairs,assignments,sampling)
     return matrices_list
 
 
 
+def generate_lattices(n):
+    domain_pairs = [[i, j] for i in range(1, n-1) for j in range(i + 1, n-1)]
 
-def plot_graph_from_adiacency(adjacency_matrix):
-    G = nx.DiGraph()
-    n = np.size(adjacency_matrix[0])
-    for i in range(n):
-        for j in range(n):
-            if adjacency_matrix[i][j] == 1:
-                G.add_edge(i,j)
+    if n > max_lattices_cardinality:
+        sampling = True
+    else:
+        sampling = False
 
-    # G = nx.transitive_reduction(G)
+    matrices_list_for_cardinality = generate_LoQ_matrices(n,domain_pairs,sampling,lattices_to_generate)
+    return matrices_list_for_cardinality
 
-    # print(TR.adj)
-
-    # pos = nx.nx_pydot.pydot_layout(,adjacency_matrix,root=0)
-    # degree = [n for n in G.in_degree()]
-    # print(degree)
-
-    # nx.draw(TR, labels={i:str(i) for i in range(n)}, pos=pos)
-    nx.draw(G, labels={i:str(i) for i in range(n)})
-    plt.show()
+def generate_all_lattices(n):
+    lattices_list = []
+    for i in range(2,n+1):
+        lattices_list += generate_lattices(i)
+    return lattices_list
 
 
 
 
+def prepare_dataset_json(lattices):
+    # FIELD: graph_cardinality; LoQ_mat; adj_mat; distribut; modularity
 
-lattices = generate_all_lattices(9)
+    with open("sample.json", "w") as outfile:
+        for i, latt in enumerate(lattices):
+            dictionary = {
+                "ID": "G"+str(i),
+                "Cardinality": np.size(latt[0]),
+                "LoQ_matrix": latt.tolist(),
+                "Adj_matrix": LoQ2Adj(latt).tolist(),
+                "Distributive": is_distributive(latt),
+                "Modular": is_modular(latt)
+            }
+            # create and write json lattice
+            json_object = json.dumps(dictionary)
+            outfile.write(json_object)
+
+    outfile.close()
+
+
+
+lattices = generate_all_lattices(5)
+
+prepare_dataset_json(lattices)
+
 
 
 for i in lattices:
--- utils.py
+++ utils.py
@@ -1,5 +1,13 @@
 import numpy as np
+import networkx as nx
+import matplotlib.pyplot as plt
 
+#TODO
+def is_modular(mat):
+    n = np.size(mat[0])
+
+    mod = True
+    return mod
 
 def is_distributive(mat):
     n = np.size(mat[0])
@@ -47,3 +55,25 @@ def join(a,b,less_or_equal):
     majority = [c for c in range(n) if (less_or_equal[a,c] == 1 and less_or_equal[b,c] == 1)]
     sup = min(majority)
     return sup
+
+
+
+def plot_graph_from_adiacency(adjacency_matrix):
+    G = nx.DiGraph()
+    n = np.size(adjacency_matrix[0])
+    for i in range(n):
+        for j in range(n):
+            if adjacency_matrix[i][j] == 1:
+                G.add_edge(i,j)
+
+    TR = nx.transitive_reduction(G)
+
+    # print(TR.adj)
+
+    # pos = nx.nx_pydot.pydot_layout(,adjacency_matrix,root=0)
+    # degree = [n for n in G.in_degree()]
+    # print(degree)
+
+    # nx.draw(TR, labels={i:str(i) for i in range(n)}, pos=pos)
+    nx.draw(TR, labels={i:str(i) for i in range(n)})
+    plt.show()
