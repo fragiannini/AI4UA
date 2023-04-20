15f5cfc9a13ce4c0cd3efb158c785ceda4b1e408
pietro <barbiero@tutanota.com>
2023-04-20 09:24:55 +0100

    Add model script with black box and skeleton of interpretable GNN 🌟

--- experiments/run.py
+++ experiments/run.py
@@ -4,119 +4,12 @@ from sklearn.preprocessing import LabelEncoder
 import pandas as pd
 import torch
 import os
-import torch.nn.functional as F
-from torch_geometric.nn import GINConv, global_mean_pool, global_add_pool
-from torch_geometric.nn.norm import BatchNorm
-from torch.nn import Linear
 
 from gnn4ua.datasets.loader import load_data
+from gnn4ua.models import BlackBoxGNN, GCoRe
+
 
 # torch.autograd.set_detect_anomaly(True)
-
-
-class GCoRe(torch.nn.Module):
-    def __init__(self, in_features, emb_size, n_classes):
-        super(GCoRe, self).__init__()
-        self.nn1 = torch.nn.Sequential(
-            Linear(in_features, emb_size),
-            torch.nn.LeakyReLU(),
-            Linear(emb_size, emb_size),
-        )
-        self.nn2 = torch.nn.Sequential(
-            Linear(emb_size, emb_size),
-            torch.nn.LeakyReLU(),
-            Linear(emb_size, emb_size),
-        )
-        self.nn3 = torch.nn.Sequential(
-            Linear(emb_size, emb_size),
-            torch.nn.LeakyReLU(),
-            Linear(emb_size, emb_size),
-        )
-        self.nn4 = torch.nn.Sequential(
-            Linear(emb_size, emb_size),
-            torch.nn.LeakyReLU(),
-            Linear(emb_size, emb_size),
-        )
-        self.nn5 = torch.nn.Sequential(
-            Linear(emb_size, emb_size),
-            torch.nn.LeakyReLU(),
-            Linear(emb_size, emb_size),
-        )
-        self.nn6 = torch.nn.Sequential(
-            Linear(emb_size, emb_size),
-            torch.nn.LeakyReLU(),
-            Linear(emb_size, emb_size),
-        )
-        self.nn7 = torch.nn.Sequential(
-            Linear(emb_size, emb_size),
-            torch.nn.LeakyReLU(),
-            Linear(emb_size, emb_size),
-        )
-        self.nn8 = torch.nn.Sequential(
-            Linear(emb_size, emb_size),
-            torch.nn.LeakyReLU(),
-            Linear(emb_size, emb_size),
-        )
-        self.conv1 = GINConv(self.nn1)
-        self.conv2 = GINConv(self.nn2)
-        self.conv3 = GINConv(self.nn3)
-        self.conv4 = GINConv(self.nn4)
-        self.conv5 = GINConv(self.nn5)
-        self.conv6 = GINConv(self.nn6)
-        self.conv7 = GINConv(self.nn7)
-        self.conv8 = GINConv(self.nn8)
-        self.bn1 = BatchNorm(emb_size)
-        self.bn2 = BatchNorm(emb_size)
-        self.bn3 = BatchNorm(emb_size)
-        self.bn4 = BatchNorm(emb_size)
-        self.bn5 = BatchNorm(emb_size)
-        self.bn6 = BatchNorm(emb_size)
-        self.bn7 = BatchNorm(emb_size)
-        self.bn8 = BatchNorm(emb_size)
-        self.linear1 = Linear(emb_size, emb_size)
-        self.linear2 = Linear(emb_size, n_classes)
-
-    def forward(self, data):
-        x, edge_index, batch = data.x, data.edge_index, data.batch
-
-        x = self.conv1(x, edge_index)
-        x = self.bn1(x)
-        x = F.leaky_relu(x)
-
-        x = self.conv2(x, edge_index)
-        x = self.bn2(x)
-        x = F.leaky_relu(x)
-
-        x = self.conv3(x, edge_index)
-        x = self.bn3(x)
-        x = F.leaky_relu(x)
-
-        x = self.conv4(x, edge_index)
-        x = self.bn4(x)
-        x = F.leaky_relu(x)
-
-        x = self.conv5(x, edge_index)
-        x = self.bn5(x)
-        x = F.leaky_relu(x)
-
-        x = self.conv6(x, edge_index)
-        x = self.bn6(x)
-        x = F.leaky_relu(x)
-
-        x = self.conv7(x, edge_index)
-        x = self.bn7(x)
-        x = F.leaky_relu(x)
-
-        x = self.conv8(x, edge_index)
-        x = self.bn8(x)
-
-        x = global_add_pool(x, batch)
-
-        x = self.linear1(x)
-        x = F.leaky_relu(x)
-        x = self.linear2(x)
-
-        return torch.softmax(x, dim=-1)
 
 
 def main():
@@ -145,11 +38,10 @@ def main():
 
         for fold, (train_index, test_index) in enumerate(skf.split(data.y, y_new)):
             # define model
-            gnn = GCoRe(data.x.shape[1], emb_size, n_classes)
+            gnn = GCoRe(data.x.shape[1], emb_size, n_classes, n_layers=8)
 
             optimizer = torch.optim.AdamW(gnn.parameters(), lr=learning_rate)
             loss_form = torch.nn.CrossEntropyLoss(weight=1-data.y.float().mean(dim=0))
-            # loss_form = torch.nn.CrossEntropyLoss()
             gnn.train()
             for epoch in range(train_epochs):
                 optimizer.zero_grad()
---
+++ gnn4ua/models.py
@@ -0,0 +1,135 @@
+import abc
+
+import torch
+import torch.nn.functional as F
+from torch_geometric.nn import GINConv, global_mean_pool, global_add_pool, Sequential
+from torch_geometric.nn.norm import BatchNorm
+from torch.nn import Linear, ModuleList, LeakyReLU
+
+
+class GraphNet(torch.nn.Module):
+    def __init__(self, in_features, emb_size, n_classes, n_layers=8):
+        super(GraphNet, self).__init__()
+        self.in_features = in_features
+        self.n_classes = n_classes
+        self.emb_size = emb_size
+        self.n_layers = n_layers
+
+
+    @abc.abstractmethod
+    def forward(self, data):
+        raise NotImplementedError
+
+
+class GCoRe(GraphNet):
+    def __init__(self, in_features, emb_size, n_classes, n_layers=8):
+        super(GCoRe, self).__init__(in_features, emb_size, n_classes, n_layers)
+
+        in_size = in_features
+        self.graph_layers = ModuleList()
+        for i in range(self.n_layers-1):
+            self.graph_layers.append(Sequential(
+                'x, edge_index', [
+                    (GINConv(
+                        torch.nn.Sequential(
+                            Linear(in_size, emb_size),
+                            LeakyReLU(),
+                            Linear(emb_size, emb_size)
+                        )
+                    ), 'x, edge_index -> x'),
+                    BatchNorm(emb_size),
+                    LeakyReLU(inplace=True),
+                ]
+            ))
+            in_size = emb_size
+
+        # for the last graph layer we don't want to apply an activation but rather a pooling layer
+        self.graph_layers.append(Sequential(
+                'x, edge_index', [
+                    (GINConv(
+                        torch.nn.Sequential(
+                            Linear(in_size, emb_size),
+                            LeakyReLU(),
+                            Linear(emb_size, emb_size)
+                        )
+                    ), 'x, edge_index -> x'),
+                    BatchNorm(emb_size),
+                ]
+            ))
+
+        # TODO: here we want to add a gumbel softmax layer to extract concepts
+
+        # last, we apply a set of linear layers to get the final prediction
+        self.dense_layers = torch.nn.Sequential(
+            Linear(emb_size, emb_size),
+            torch.nn.LeakyReLU(),
+            Linear(emb_size, n_classes)
+        )
+
+    def forward(self, data):
+        x, edge_index, batch = data.x, data.edge_index, data.batch
+
+        for i, layer in enumerate(self.graph_layers):
+            x = layer(x, edge_index)
+
+        x = global_add_pool(x, batch)
+        # TODO: here we want to add a gumbel softmax layer to extract concepts
+
+        x = self.dense_layers(x)
+
+        return torch.softmax(x, dim=-1)
+
+
+class BlackBoxGNN(GraphNet):
+    def __init__(self, in_features, emb_size, n_classes, n_layers=8):
+        super(BlackBoxGNN, self).__init__(in_features, emb_size, n_classes, n_layers)
+
+        in_size = in_features
+        self.graph_layers = ModuleList()
+        for i in range(self.n_layers-1):
+            self.graph_layers.append(Sequential(
+                'x, edge_index', [
+                    (GINConv(
+                        torch.nn.Sequential(
+                            Linear(in_size, emb_size),
+                            LeakyReLU(),
+                            Linear(emb_size, emb_size)
+                        )
+                    ), 'x, edge_index -> x'),
+                    BatchNorm(emb_size),
+                    LeakyReLU(inplace=True),
+                ]
+            ))
+            in_size = emb_size
+
+        # for the last graph layer we don't want to apply an activation but rather a pooling layer
+        self.graph_layers.append(Sequential(
+                'x, edge_index', [
+                    (GINConv(
+                        torch.nn.Sequential(
+                            Linear(in_size, emb_size),
+                            LeakyReLU(),
+                            Linear(emb_size, emb_size)
+                        )
+                    ), 'x, edge_index -> x'),
+                    BatchNorm(emb_size),
+                ]
+            ))
+
+        # last, we apply a set of linear layers to get the final prediction
+        self.dense_layers = torch.nn.Sequential(
+            Linear(emb_size, emb_size),
+            torch.nn.LeakyReLU(),
+            Linear(emb_size, n_classes)
+        )
+
+    def forward(self, data):
+        x, edge_index, batch = data.x, data.edge_index, data.batch
+
+        for i, layer in enumerate(self.graph_layers):
+            x = layer(x, edge_index)
+
+        x = global_add_pool(x, batch)
+        x = self.dense_layers(x)
+
+        return torch.softmax(x, dim=-1)
