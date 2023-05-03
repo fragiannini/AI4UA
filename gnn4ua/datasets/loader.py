b455673807b0293c2aa1d78038b0b54aed18e064
pietro <barbiero@tutanota.com>
2023-05-03 09:56:14 +0200

    Add option to load data to assess strong generalization 🌟

--- gnn4ua/datasets/loader.py
+++ gnn4ua/datasets/loader.py
@@ -6,7 +6,8 @@ import numpy as np
 import torch_geometric as pyg
 
 
-def load_data(dataset_name, label_name, root_dir='../gnn4ua/datasets/'):
+def load_data(dataset_name, label_name, root_dir='../gnn4ua/datasets/', generalization='strong',
+              random_state=42, max_size_train=40, max_prob_train=0.8):
     file_name = os.path.join(root_dir, f'{dataset_name}_{label_name}.joblib')
 
     if not os.path.exists(file_name):
@@ -21,15 +22,40 @@ def load_data(dataset_name, label_name, root_dir='../gnn4ua/datasets/'):
         edge_indices = []
         labels = []
         batch = []
+        train_mask = []
+        test_mask = []
         n_nodes_seen = 0
         for i, matrix in enumerate(adjacency_matrices):
+            # TODO: make matrix symmetric?
+            # generate the indices of the nonzero elements in the adjacency matrix
             matrix_with_self_loops = np.array(matrix) + np.eye(len(matrix))
             matrix_indices = torch.nonzero(torch.tensor(matrix_with_self_loops, dtype=torch.float)).tolist()
             matrix_indices_shifted = [(index[0] + n_nodes_seen, index[1] + n_nodes_seen) for index in matrix_indices]
             edge_indices.extend(matrix_indices_shifted)
+
+            # generate the labels
             label = df[label_name].values[i].astype(int)
             labels.append(torch.LongTensor([1-label, label]))
+
+            # generate the batch index
             batch.extend(torch.LongTensor([i] * len(matrix)))
+
+            # generate the train/test masks
+            if generalization == 'strong':
+                if len(matrix) > max_size_train:
+                    is_train_graph = False
+                elif len(matrix) < max_size_train:
+                    is_train_graph = True
+                elif len(matrix) == max_size_train:
+                    if np.random.RandomState(random_state*(i+1)).rand() < max_prob_train:
+                        is_train_graph = True
+                    else:
+                        is_train_graph = False
+            else:
+                is_train_graph = True if np.random.RandomState(random_state*(i+1)).rand() < max_prob_train else False
+            train_mask.extend(torch.BoolTensor([is_train_graph]))
+            test_mask.extend(torch.BoolTensor([not is_train_graph]))
+
             n_nodes_seen += len(matrix)
 
         # Create the edge index tensor
@@ -37,12 +63,14 @@ def load_data(dataset_name, label_name, root_dir='../gnn4ua/datasets/'):
         x = torch.ones((edge_index.max().item() + 1, 1))
         y = torch.vstack(labels)
         batch = torch.hstack(batch)
+        train_mask = torch.hstack(train_mask)
+        test_mask = torch.hstack(test_mask)
 
-        data = pyg.data.Data(x=x, edge_index=edge_index, y=y, batch=batch)
+        data = pyg.data.Data(x=x, edge_index=edge_index, y=y, batch=batch, train_mask=train_mask, test_mask=test_mask)
         data.validate(raise_on_error=True)
 
-        joblib.dump(data, os.path.join(root_dir, f'{dataset_name}_{label_name}.joblib'))
+        joblib.dump(data, os.path.join(root_dir, f'{dataset_name}_{label_name}_{generalization}.joblib'))
     else:
-        data = joblib.load(os.path.join(root_dir, f'{dataset_name}_{label_name}.joblib'))
+        data = joblib.load(os.path.join(root_dir, f'{dataset_name}_{label_name}_{generalization}.joblib'))
         data.validate(raise_on_error=True)
     return data
