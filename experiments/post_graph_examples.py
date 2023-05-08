baae8e19926f9bb79769c7b137c233c3c4191b3a
pietro <barbiero@tutanota.com>
2023-05-08 15:37:03 +0200

    Visualize graph-level subgraphs in embedding space 🌟💩

---
+++ experiments/post_graph_examples.py
@@ -0,0 +1,128 @@
+import torch
+import os
+
+from matplotlib.colors import ListedColormap
+from torch_geometric import seed_everything
+import numpy as np
+import seaborn as sns
+import sys
+import matplotlib.pyplot as plt
+import torch_geometric as pyg
+
+sys.path.append('..')
+from gnn4ua.datasets.loader import load_data
+from gnn4ua.models import BlackBoxGNN, GCoRe, HierarchicalGCN
+from gnn4ua.viz import visualize_lattice, get_samples_to_plot, visualize_concept_space
+
+# torch.autograd.set_detect_anomaly(True)
+seed_everything(42)
+
+# viz parameters
+n_examples_per_concept = 5
+n_concepts = 20
+n_hops_neighborhood = 8
+plt.rcParams.update({
+    "text.usetex": True,
+})
+
+def main():
+    # hyperparameters
+    random_states = np.random.RandomState(42).randint(0, 1000, size=3)
+    dataset = 'samples_50_saved'
+    temperature = 1
+    full_names = ["Distributive", "Modular", "Meet_SemiDistributive", "Join_SemiDistributive", "SemiDistributive"]
+    all_labels = ["D", "M", "MSD", "JSD", "SD"]
+    label_names = ["multilabel"]
+    generalization_modes = ['weak']
+    train_epochs = 200
+    emb_size = 16
+    learning_rate = 0.005
+    max_size_train = 8
+    max_prob_train = 0.8
+    n_layers = 8
+    internal_loss_weight = 0.1
+
+    # we will save all results in this directory
+    results_dir = f"results/"
+    os.makedirs(results_dir, exist_ok=True)
+
+    results = []
+    cols = ['task', 'generalization', 'model', 'test_auc', 'train_auc', 'n_layers', 'temperature', 'emb_size', 'learning_rate', 'train_epochs', 'max_size_train', 'max_prob_train']
+    for label_name in label_names:
+        model_dir = os.path.join(results_dir, f'task_{label_name}/models/')
+        os.makedirs(model_dir, exist_ok=True)
+        figures_dir = os.path.join(results_dir, f'task_{label_name}/figures/')
+        os.makedirs(figures_dir, exist_ok=True)
+
+        for generalization in generalization_modes:
+            for state_id, random_state in enumerate(random_states):
+
+                # load data and set up cross-validation
+                data = load_data(dataset, label_name=label_name, root_dir='../gnn4ua/datasets/',
+                                 generalization=generalization, random_state=random_state,
+                                 max_size_train=max_size_train, max_prob_train=max_prob_train)
+                train_index, test_index = data.train_mask, data.test_mask
+                print(sum(train_index), sum(test_index))
+
+                # reset model weights for each fold
+                models = [
+                    HierarchicalGCN(data.x.shape[1], emb_size, data.y.shape[1], n_layers),
+                    # GCoRe(data.x.shape[1], emb_size, data.y.shape[1], n_layers),
+                    # BlackBoxGNN(data.x.shape[1], emb_size, data.y.shape[1], n_layers),
+                ]
+
+                for gnn in models:
+                    model_path = os.path.join(model_dir, f'{gnn.__class__.__name__}_generalization_{generalization}_seed_{random_state}_temperature_{temperature}_embsize_{emb_size}.pt')
+                    print(f'Running {gnn.__class__.__name__} on {label_name} ({generalization} generalization) [seed {state_id+1}/{len(random_states)}]')
+
+                    if not os.path.exists(model_path):
+                        raise ValueError(f'Model {model_path} does not exist')
+
+                    figures_dir = os.path.join(results_dir, f'figures/{label_name}_{generalization}/')
+                    os.makedirs(figures_dir, exist_ok=True)
+                    summary_figures_dir = os.path.join(results_dir, f'figures/')
+                    os.makedirs(summary_figures_dir, exist_ok=True)
+
+                    # get model predictions
+                    gnn.load_state_dict(torch.load(model_path))
+                    for param in gnn.parameters():
+                        param.requires_grad = False
+                    y_pred, node_concepts, graph_concepts = gnn.forward(data)
+
+                    plt.figure(figsize=[6, 2])
+                    plt.title(f'Interpretable Classifier Weights', fontsize=16)
+                    sns.heatmap(gnn.dense_layers[0].weight, cmap='vlag', square=True)
+                    plt.xlabel('Concept ID', fontsize=12)
+                    plt.ylabel('Task label', fontsize=12)
+                    plt.yticks(ticks=np.arange(0, 5)+0.5, labels=all_labels, rotation=0, fontsize=12)
+                    plt.tight_layout()
+                    plt.savefig(os.path.join(summary_figures_dir, f'classifier_weights.png'))
+                    plt.savefig(os.path.join(summary_figures_dir, f'classifier_weights.pdf'))
+                    plt.show()
+
+                    layer_ids = [7, 2]
+                    cols = sns.color_palette("colorblind")[:2]
+                    cmap = ListedColormap(sns.color_palette(cols).as_hex())
+                    for layer_id in layer_ids:
+                        for task_id in range(len(all_labels)):
+                            for concept_idx in range(5):
+                                samples2d, saved_imgs, concepts2d, correct_pred_mask, cid = get_samples_to_plot(gnn, data, task_id, layer_id, figures_dir, all_labels, concept_idx, n_concepts, random_state)
+
+                                base_name = full_names[task_id].replace('_', ' ')
+                                fig, ax = plt.subplots(figsize=(6, 4))
+                                plt.title(f'Concept {cid.item()} [L{layer_id}]', fontsize=18)
+                                visualize_concept_space(samples2d, saved_imgs, concepts2d, correct_pred_mask, data, task_id, full_names, cmap, cols, ax)
+                                plt.xlabel('UMAP 1st component', fontsize=14)
+                                plt.ylabel('UMAP 2nd component', fontsize=14)
+                                sns.despine(offset=0)
+                                plt.tight_layout()
+                                plt.savefig(os.path.join(summary_figures_dir, f'task_{label_name}_{base_name}_layer_{layer_id}_cid_{cid.item()}.png'))
+                                plt.savefig(os.path.join(summary_figures_dir, f'task_{label_name}_{base_name}_layer_{layer_id}_cid_{cid.item()}.pdf'))
+                                plt.show()
+
+                    break
+                break
+
+
+if __name__ == '__main__':
+    main()
--- gnn4ua/viz.py
+++ gnn4ua/viz.py
@@ -2,8 +2,122 @@ import torch_geometric as pyg
 import matplotlib.pyplot as plt
 import networkx as nx
 from networkx.drawing.nx_agraph import graphviz_layout
+from matplotlib.offsetbox import OffsetImage, AnnotationBbox
+import numpy as np
+from torch_geometric import seed_everything
 
-from .utils import find_k_hop_subgraph
+from gnn4ua.utils import find_cluster_centroids
+from sklearn.manifold import TSNE
+from matplotlib.image import imread
+from matplotlib.offsetbox import OffsetImage, AnnotationBbox
+from sklearn.preprocessing import LabelEncoder
+from matplotlib.patches import Patch
+import umap
+import torch
+from matplotlib.colors import ListedColormap
+from sklearn.cluster import KMeans
+import os
+import seaborn as sns
+
+from .utils import find_k_hop_subgraph, pyg_edges_to_nx_lattice
+
+
+def get_samples_to_plot(gnn, data, task_id, layer_id, figures_dir, all_labels,
+                            concept_idx=0, n_concepts=5, random_state=42):
+    seed_everything(random_state)
+    y_pred, node_concepts, graph_concepts = gnn.forward(data)
+    concepts = graph_concepts[layer_id]
+    edge_indexes = pyg.utils.unbatch_edge_index(data.edge_index, data.batch)
+    graph_sizes = torch.LongTensor([len(torch.unique(eidx.ravel())) for eidx in edge_indexes])
+
+    concept_id = torch.argsort(gnn.dense_layers[0].weight, dim=1, descending=False)[task_id, concept_idx]
+    correct_pred_mask = data.y[:, task_id] == (y_pred[:, task_id] > 0)
+
+    reducer = umap.UMAP(metric='jaccard', random_state=random_state)
+    reducer.fit(concepts[correct_pred_mask].detach().numpy(), y=data.y[correct_pred_mask, task_id])
+    concepts2d = reducer.transform(concepts[correct_pred_mask].detach().numpy())
+
+    saved_imgs = []
+    samples_to_plot = []
+    for task_state in [0, 1]:
+        concept_mask = (concepts[:, concept_id] > 0) & \
+                       (data.y[:, task_id] == task_state) & \
+                       correct_pred_mask  # omission of these lattices is relevant
+        concept_indexes = torch.argwhere(concept_mask).flatten()
+        graph_sizes_sorted_idx = torch.argsort(graph_sizes[concept_indexes].long(), descending=False)
+        concept_indexes_sorted = concept_indexes[graph_sizes_sorted_idx][:n_concepts]
+
+        nc = 3 if task_state == 0 else 1
+        if len(concept_indexes_sorted) < nc:
+            print('Not enough concepts to plot')
+            continue
+        rnd_idx = np.random.RandomState(random_state).choice(len(concept_indexes_sorted), size=nc, replace=False)
+        for graph_id in concept_indexes_sorted[rnd_idx]:
+            edge_index = edge_indexes[graph_id]
+            plt.figure(figsize=[2.5, 2.5])
+            visualize_lattice(edge_index, node_size=300)
+            plt.tight_layout()
+            plt.savefig(os.path.join(figures_dir, f'task_{all_labels[task_id]}_layer_{layer_id}_concept_{int(concept_id)}.png'), transparent=True, dpi=20)
+            plt.draw()
+            saved_img = imread(os.path.join(figures_dir, f'task_{all_labels[task_id]}_layer_{layer_id}_concept_{int(concept_id)}.png'))
+            saved_imgs.append(saved_img)
+            samples_to_plot.append(concepts[graph_id])
+
+    samples_to_plot = torch.stack(samples_to_plot)
+    samples2d = reducer.transform(samples_to_plot.detach().numpy())
+    return samples2d, saved_imgs, concepts2d, correct_pred_mask, concept_id
+
+
+def visualize_concept_space(samples2d, saved_imgs, concepts2d, correct_pred_mask, data, task_id, full_names, cmap, cols, ax):
+    legend_patches = []
+    # plt.scatter(concepts2d[:, 0], concepts2d[:, 1], c=data.y[:, task_id], alpha=0.01, marker='.', s=5,
+    #             edgecolors=None, clip_on=False, cmap=cmap)
+    sns.kdeplot(x=concepts2d[:, 0], y=concepts2d[:, 1], hue=data.y[correct_pred_mask, task_id] > 0, fill=False,
+                alpha=0.15, linewidth=0, clip_on=False, cmap=cmap)
+    # sns.scatterplot(x=concepts2d[:, 0], y=concepts2d[:, 1], hue=data.y[correct_pred_mask, task_id] > 0, marker='.',
+    #                 alpha=0.005, cmap=cmap)
+
+    base_name = full_names[task_id].replace('_', ' ')
+    for col, l_name in zip(cols, ['Non-' + base_name, base_name]):
+        legend_patches.append(Patch(facecolor=col, label=l_name, alpha=0.4))
+    # plt.scatter(samples2d[:, 0], samples2d[:, 1], s=1500, c=cols[0], alpha=0.3, clip_on=False)
+    for sample_id in range(len(samples2d)):
+        imagebox = OffsetImage(saved_imgs[sample_id], zoom=.7)
+        ab = AnnotationBbox(imagebox, samples2d[sample_id], frameon=False)
+        ax.add_artist(ab)
+    plt.legend(handles=legend_patches, fontsize=12)
+
+
+def visualize_lattice(edge_index, node_size=200, node_color='k', with_labels=False):
+    digraph = pyg_edges_to_nx_lattice(edge_index)
+    # try:
+    #     nx.find_cycle(digraph, orientation='ignore')
+    pos = graphviz_layout(digraph, prog='dot')
+    nx.draw_networkx(digraph, pos=pos, node_size=node_size, node_color=node_color, with_labels=with_labels)
+    plt.gca().invert_yaxis()
+    plt.axis('off')
+    plt.tight_layout()
+    # except:
+    #     print('Lattice is trivial')
+
+
+def imscatter(x, y, image, ax=None, zoom=1):
+    if ax is None:
+        ax = plt.gca()
+    try:
+        image = plt.imread(image)
+    except TypeError:
+        # Likely already an array...
+        pass
+    im = OffsetImage(image, zoom=zoom)
+    x, y = np.atleast_1d(x, y)
+    artists = []
+    for x0, y0 in zip(x, y):
+        ab = AnnotationBbox(im, (x0, y0), xycoords='data', frameon=False)
+        artists.append(ax.add_artist(ab))
+    ax.update_datalim(np.column_stack([x, y]))
+    ax.autoscale()
+    return artists
 
 
 def visualize_concept(nearest_neighbors_index, edge_index, batch_index,
@@ -19,6 +133,7 @@ def visualize_concept(nearest_neighbors_index, edge_index, batch_index,
         pos = graphviz_layout(digraph, prog='dot')
         plt.subplot(1, n_examples_per_concept, example_id + 1)
         nx.draw_networkx(digraph, pos=pos, node_size=node_size, node_color=node_color, with_labels=with_labels)
+        plt.gca().invert_yaxis()
         plt.axis('off')
         plt.tight_layout()
     return
